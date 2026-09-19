0b33939aca83e1b32206e9809f83fb7e298177da
GnanaJeffersonP <pjeffersondgl@gmail.com>
2026-09-19 18:36:06 +0530

    Fix mobile side navigation

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ var sidenav=document.getElementById("sidenav")
 var close=document.getElementById("close")
 var menu=document.getElementById("menu")
  
-menu.addEventListener("click",function(){
-    sidenav.style.right="0"
+menu.addEventListener("click", function(){
+    sidenav.classList.remove("translate-x-full");
 })
-close.addEventListener("click",function(){
-    sidenav.style.right="-100%"
+
+close.addEventListener("click", function(){
+    sidenav.classList.add("translate-x-full");
 })
 
 // login
